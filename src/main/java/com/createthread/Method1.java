9d358bbc1e844b851b2fcd43fd7a0eae8da7f177
jaywong <dandyjiew@gmail.com>
2021-03-30 16:35:59 +0800

    [add]新增创建线程方式1

--- src/main/java/com/createthread/Method1.java
+++ src/main/java/com/createthread/Method1.java
@@ -7,18 +7,43 @@ package com.createthread;
  * 劣势：因为线程已经继承了Thread类，所以不能再继承其他父类(https://blog.csdn.net/sinat_27933301/article/details/69944286原文链接)
  */
 public class Method1 {
-    public static void main(String[] args) throws InterruptedException {
-        MyThread myThread = new MyThread();
-        myThread.start();
+    public static void main(String[] args) {
+        MyThread1 myThread1 = new MyThread1();
+        myThread1.start();
+
         //可以发现，除了自己生成的线程运行了，其实还有隐藏的主线程main线程
         System.out.println(Thread.currentThread().getName());
+
+        MyThread2 myThread2 = new MyThread2("我的线程2");
+        myThread2.start();
+
+        //有多种方式可以给线程赋名称
+        MyThread2 myThread3 = new MyThread2();
+        myThread3.setName("我的线程3");
+        myThread3.start();
     }
 }
 
-class MyThread extends Thread {
+class MyThread1 extends Thread {
     @Override
     public void run() {
-        this.setName("我的线程");
+        this.setName("我的线程1");
+        System.out.println(this.getName() + "开始运行了");
+    }
+}
+
+class MyThread2 extends Thread {
+
+    public MyThread2() {
+
+    }
+
+    public MyThread2(String name) {
+        super(name);
+    }
+
+    @Override
+    public void run() {
         System.out.println(this.getName() + "开始运行了");
     }
 }
